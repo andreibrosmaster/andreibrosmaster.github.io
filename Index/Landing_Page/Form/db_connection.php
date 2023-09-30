dfa192844f5407e2a0cc0257948c9b4e335dca1a
andreibrosmaster <111739317+andreibrosmaster@users.noreply.github.com>
2023-09-30 19:02:39 +0800

    Add files via upload

---
+++ Index/Landing_Page/Form/db_connection.php
@@ -0,0 +1,18 @@
+<?php
+function connectDB() {
+    $db_host = "localhost"; // Change this to your database host
+    $db_user = "root"; // Change this to your database username
+    $db_pass = ""; // Change this to your database password
+    $db_name = "register"; // Change this to your database name
+
+    // Create a connection to the database
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
+?>
